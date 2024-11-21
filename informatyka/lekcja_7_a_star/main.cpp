3e9657b8ca55cfbeaafa84fa1fb945931e78cf68
Wojciech Choroś <wojciech.choros@dynamico.ai>
2024-11-21 14:14:22 +0100

    mior adjustments on the a* project

--- informatyka/lekcja_7_a_star/main.cpp
+++ informatyka/lekcja_7_a_star/main.cpp
@@ -7,7 +7,7 @@ int main() {
     // NodeField field(25, 25); // Uncomment to generate an empty field
     // field.addObstacles(50);   // Uncomment to add obstacles
 
-    NodeField field("field.txt"); // Load field from file
+    NodeField field("../field.txt"); // Load field from file
     if (!field.findPath(0, 1, 40, 3, true)) {
         cout << "No path found" << endl;
     }
